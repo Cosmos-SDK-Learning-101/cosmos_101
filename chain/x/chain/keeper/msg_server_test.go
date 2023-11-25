e7ff739313c17b14ee78b702b0eb7fad424ae402
Alvin Ichi <48800728+nhatlapross@users.noreply.github.com>
2023-11-25 13:12:30 +0700

    Add files via upload

---
+++ chain/x/chain/keeper/msg_server_test.go
@@ -0,0 +1,23 @@
+package keeper_test
+
+import (
+	"context"
+	"testing"
+
+	keepertest "chain/testutil/keeper"
+	"chain/x/chain/keeper"
+	"chain/x/chain/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.ChainKeeper(t)
+	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
+}
+
+func TestMsgServer(t *testing.T) {
+	ms, ctx := setupMsgServer(t)
+	require.NotNil(t, ms)
+	require.NotNil(t, ctx)
+}
